3533bb02652c92da3e3ef96200b48b9120573cb4
Nexusname <namedarrius@gmail.com>
2024-01-08 13:04:25 +0700

    exam1

--- 01418442-60-WebTech3/script.js
+++ 01418442-60-WebTech3/script.js
@@ -1,38 +1,67 @@
 let machineNumber = Math.ceil(Math.random() * 21);
 let score = 10;
 let highscore = 0;
+console.log("คำตอบ คือ " + machineNumber);
 
 document.querySelector(".check").addEventListener("click", () => {
-  const guess = document.querySelector(".guess").value;
-  if (guess != machineNumber) {
-    document.querySelector(".message").textContent =
-      guess < machineNumber ? "น้อยกว่า" : "มากกว่า";
-    score--;
-    document.querySelector(".score").textContent = score;
+  // if (score > 0) {
+  // 	guess = document.querySelector(".guess").value;
+  // 	if (guess == machineNumber) {
+  // 		document.querySelector(".message").textContent = "ยินดีด้วยคุณทายถูกแล้ว";
+  // 		document.querySelector(".secret").textContent = machineNumber;
+  // 		if (score > highscore) {
+  // 			highscore = score;
+  // 			document.querySelector(".highscore").textContent = score;
+  // 		}
+  // 	} else if (guess > machineNumber) {
+  // 		document.querySelector(".message").textContent =
+  // 			"เลขที่คุณทายมีค่ามากเกินไป";
+  // 		score--;
+  // 	} else if (guess < machineNumber) {
+  // 		document.querySelector(".message").textContent =
+  // 			"เลขที่คุณทายมีค่าน้อยเกินไป";
+  // 		score--;
+  // 	}
+  // 	document.querySelector(".score").textContent = score;
+  // } else {
+  // 	document.querySelector(".message").textContent = "GAME OVER";
+  // }
+  guess = document.querySelector(".guess").value;
+  if (guess < 1 || guess > 21) {
+    alert("โปรดใส่ค่าระหว่าง 1-21");
   } else {
-    document.querySelector(".message").textContent = "เย้ ชนะแล้วไอ้น้อง";
-    document.querySelector(".secret").textContent = machineNumber;
-    if (score > highscore) {
-      highscore = score;
-      document.querySelector(".highscore").textContent = highscore;
+    if (score > 0) {
+      if (guess != machineNumber) {
+        if (guess > machineNumber) {
+          document.querySelector(".message").textContent =
+            "เลขที่คุณทายมีค่ามากเกินไป";
+          score--;
+        } else if (guess < machineNumber) {
+          document.querySelector(".message").textContent =
+            "เลขที่คุณทายมีค่าน้อยเกินไป";
+          score--;
+        }
+      } else {
+        document.querySelector(".message").textContent =
+          "ยินดีด้วยคุณทายถูกแล้ว";
+        document.querySelector(".secret").textContent = machineNumber;
+        if (score > highscore) {
+          highscore = score;
+          document.querySelector(".highscore").textContent = score;
+        }
+      }
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "GAME OVER";
     }
-  }
-
-  if (score == 0) {
-    document.querySelector(".message").textContent = "เสียเครดิตแล้ว!";
-    document.querySelector(".secret").textContent = machineNumber;
-    window.open(
-      "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs",
-      "_blank"
-    );
   }
 });
 
-document.querySelector(".btn_reload").addEventListener("click", () => {
+document.querySelector(".restratBtn").addEventListener("click", () => {
   machineNumber = Math.ceil(Math.random() * 21);
-  score = 10;
   document.querySelector(".secret").textContent = "?";
+  score = 10;
   document.querySelector(".score").textContent = score;
-  document.querySelector(".message").textContent = "เอ๊า!! ทายมา";
-  document.querySelector(".guess").value = "";
+  document.querySelector(".message").textContent = "เอ้า!! ทายมา";
+  console.log("คำตอบ คือ " + machineNumber);
 });
