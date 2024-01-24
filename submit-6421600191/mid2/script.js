0dc6aa9789b30b092deb38eadb8dc843dfdb71fa
namesangjun_ <89078800+N0Z3I@users.noreply.github.com>
2024-01-24 19:36:18 +0700

    mid2

--- submit-6421600191/mid2/script.js
+++ submit-6421600191/mid2/script.js
@@ -56,3 +56,60 @@ let matchResults = [
     location: "St. James' Park, Newcastle",
   },
 ];
+
+function updateTable() {
+  const tableBody = document
+    .getElementById("resultsTable")
+    .getElementsByTagName("tbody")[0];
+
+  matchResults.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+  tableBody.innerHTML = "";
+
+  const order = ["date", "teams", "score", "location"];
+
+  for (const data of matchResults) {
+    const row = document.createElement("tr");
+
+    for (const key of order) {
+      const cell = document.createElement("td");
+
+      if (key === "teams") {
+        cell.textContent = `${data.home_team} vs ${data.away_team}`;
+      } else if (key === "date") {
+        cell.textContent = data[key];
+      } else {
+        cell.textContent = data[key];
+      }
+      row.appendChild(cell);
+    }
+    tableBody.appendChild(row);
+  }
+}
+
+function addResult() {
+  const dateInput = document.getElementById("date").value;
+  const homeTeam = document.getElementById("homeTeam").value;
+  const awayTeam = document.getElementById("awayTeam").value;
+  const score = document.getElementById("score").value;
+  const location = document.getElementById("location").value;
+
+  const newResult = {
+    date: dateInput,
+    home_team: homeTeam,
+    away_team: awayTeam,
+    score: score,
+    location: location,
+  };
+
+  matchResults.push(newResult);
+
+  document.getElementById("date").value = "";
+  document.getElementById("homeTeam").value = "";
+  document.getElementById("awayTeam").value = "";
+  document.getElementById("score").value = "";
+  document.getElementById("location").value = "";
+  updateTable();
+}
+
+updateTable();
