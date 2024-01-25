2b965e6d2dbf4a22e80aff3c458715e1b37486bc
namesangjun_ <89078800+N0Z3I@users.noreply.github.com>
2024-01-25 21:15:28 +0700

    Commend

--- submit-6421600191/mid2/script.js
+++ submit-6421600191/mid2/script.js
@@ -57,37 +57,43 @@ let matchResults = [
   },
 ];
 
-function updateTable() {
+function updateTable() {  // ฟังก์ชั่นที่ใช้อัพเดทตารางด้วย matchResults
   const tableBody = document
     .getElementById("resultsTable")
     .getElementsByTagName("tbody")[0];
-
-  matchResults.sort((a, b) => new Date(b.date) - new Date(a.date));
+  // ใช้เรียงลำดับผลการแข่งขันตาม วัน เดือน ปี
+  matchResults.sort((a, b) => new Date(b.date) - new Date(a.date)); 
 
   tableBody.innerHTML = "";
 
+  // Array ขอตัวที่ต้องการเรียกใช้
   const order = ["date", "teams", "score", "location"];
 
+  // Loop เพื่อแสดงผลการแข่งใน matchResults
   for (const data of matchResults) {
     const row = document.createElement("tr");
 
     for (const key of order) {
       const cell = document.createElement("td");
 
+  // เป็นเงื่อนไขที่กำหนดเพื่อให้ home_team และ away_team เป็น key เดียวกันเพราะเวลาแสดงตารางแล้วมันแยก column กัน
       if (key === "teams") {
         cell.textContent = `${data.home_team} vs ${data.away_team}`;
       } else if (key === "date") {
+        // ใช้ในการจัดรูปแบบวันที่
         cell.textContent = data[key];
       } else {
         cell.textContent = data[key];
       }
       row.appendChild(cell);
     }
+    // มีไว้เพื่อรวม row ใหม่ที่สร้างขึ้นเข้ากับเนื้อหาของตารางเก่า
     tableBody.appendChild(row);
   }
 }
 
-function addResult() {
+function addResult() { // ฟังก์ชั่นที่ใช้เพิ่ม results ใหม่ลงไปในตาราง
+  // รับ values จาก form
   const dateInput = document.getElementById("date").value;
   const homeTeam = document.getElementById("homeTeam").value;
   const awayTeam = document.getElementById("awayTeam").value;
@@ -104,12 +110,15 @@ function addResult() {
 
   matchResults.push(newResult);
 
+  // มีไว้เพื่อ clear ช่อง input หลังจากกด add result
   document.getElementById("date").value = "";
   document.getElementById("homeTeam").value = "";
   document.getElementById("awayTeam").value = "";
   document.getElementById("score").value = "";
   document.getElementById("location").value = "";
+
+  // อัปเดตตารางด้วย ข้อมูลใหม้ และ ข้อมูลเก่า
   updateTable();
 }
-
+// เรียก updateTable เมื่อโหลดหน้า web ครั้งแรกเพื่อให้แสดงตารางมาก่อน
 updateTable();
