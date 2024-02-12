cf07abf659dcbe8e07dcc078f9898b15a7b6910e
PONDRICK <99117013+PONDRICK@users.noreply.github.com>
2024-02-12 14:25:59 +0700

    new  commit

--- 01418442-60-WebTech6/script.js
+++ 01418442-60-WebTech6/script.js
@@ -1,41 +1,53 @@
 "use strict";
 
 const countries = document.querySelector(".countries");
+const countryInput = document.getElementById("countryInput");
+const searchBtn = document.getElementById("searchBtn");
 
 const getCountry = function (country) {
-	console.log(country);
-	const req = new XMLHttpRequest();
-	req.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-	req.send();
+  const req = new XMLHttpRequest();
+  req.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  req.send();
 
-	req.addEventListener("load", function () {
-		const [data] = JSON.parse(this.responseText);
-		console.log(data);
-		console.log(data.name.common);
-		const html = `
+  req.addEventListener("load", function () {
+    if (req.status >= 200 && req.status < 300) {
+      const [data] = JSON.parse(this.responseText);
+      const html = `
         <article class="country">
-            <img src="${data.flags.png}" alt="" class="country_img" />
-            <div class="country_data">
-                <h3 class="country_name">${data.name.common}</h3>
-                <h4 class="country_region">${data.region}</h4>
-                <p class="country_row">🗣️</span> ${Object.values(data.languages)}</p>
-                <p class="country_row">💰</span> ${Object.values(data.currencies)[0].name}</p>
-                <p class="country_row">💰</span> ${Object.values(data.population)}</p>
-                <p class="country_row">💰</span> ${Object.values(data.borders)}</p>
-                <p class="country_row">🏢</span> ${Object.values(data.capital)}</p>
-            </div>
+          <img src="${
+            data.flags.png
+          }" alt="${country} flag" class="country_img" />
+          <div class="country_data">
+            <h3 class="country_name">${data.name.common}</h3>
+            <h4 class="country_region">${data.region}</h4>
+            <p class="country_row"><span>🗣️</span>${Object.values(
+              data.languages
+            ).join(", ")}</p>
+            <p class="country_row"><span>💵</span>${
+              Object.values(data.currencies)[0].name
+            }</p>
+          </div>
         </article>`;
-		countries.insertAdjacentHTML("beforeend", html);
-		countries.style.opacity = 1;
-	});
+      countries.insertAdjacentHTML("beforeend", html);
+      countries.style.opacity = 1;
+    } else {
+      console.error("Failed to fetch country data");
+    }
+  });
 };
+
+// Event listener for the search button
+searchBtn.addEventListener("click", function () {
+  const countryName = countryInput.value.trim();
+  if (countryName !== "") {
+    // Clear previous search results
+    countries.innerHTML = "";
+    getCountry(countryName);
+  }
+});
+
+// Initial country fetch examples
+getCountry("thailand");
 getCountry("japan");
+getCountry("france");
 getCountry("USA");
-getCountry("France");
-getCountry("Egypt");
-getCountry("Australia");
-// const emptySpace = document.querySelector(".space");
-
-// const html = `<h2>New Message:</h2><p>this is a message from script</p>`;
-
-// emptySpace.insertAdjacentHTML("beforeend", html);
